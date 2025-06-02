601cbe0c0c3f1f154b10519256a407b88f45cf95
JoaoEmanoel2005 <jeas9999@gmail.com>
2025-06-02 07:31:58 -0300

    finalizacao do tema por parte do codigo

--- footer.php
+++ footer.php
@@ -1,6 +1,15 @@
 <footer>
   <div class="container">
     <p>&copy; <?php echo date('Y'); ?> João Emanoel. Todos os direitos reservados.</p>
+    
+    <div class="contact-links">
+      <a href="https://wa.me/12992524075" target="_blank" title="WhatsApp">
+        <i class="fab fa-whatsapp"></i> WhatsApp
+      </a>
+      <a href="https://github.com/JoaoEmanoel2005" target="_blank" title="GitHub">
+        <i class="fab fa-github"></i> GitHub
+      </a>
+    </div>
   </div>
 </footer>
 
